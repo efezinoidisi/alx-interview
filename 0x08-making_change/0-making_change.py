cd208907b6ad976e5e9f2c52a582e92a3aacdd9d
efezino idisi <enduranceidisi13@gmail.com>
2023-09-21 17:11:59 +0100

    fix: return 0 instead of none when total is 0 or less

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,7 +7,7 @@ This module contains the make change function
 def makeChange(coins, total):
     """returns fewest number of coins to meet total"""
     if total <= 0:
-        return
+        return 0
 
     min_coins = [0] + [total + 1] * total
 
