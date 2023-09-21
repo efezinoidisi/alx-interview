4ac5222f336b71b677bb20e6c50886988a761c17
efezino idisi <enduranceidisi13@gmail.com>
2023-09-21 17:03:03 +0100

    complete making change problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains the make change function
+"""
+
+
+def makeChange(coins, total):
+    """returns fewest number of coins to meet total"""
+    if total <= 0:
+        return
+
+    min_coins = [0] + [total + 1] * total
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+
+    if min_coins[total] < total:
+        return min_coins[total]
+
+    return -1
